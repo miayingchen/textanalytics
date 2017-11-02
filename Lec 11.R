4d8bbd0f9445190b1fdc13b86fc8837f7bce5a56
miayingchen <miayingchen@gmail.com>
2017-11-01 20:21:00 -0700

    midway through the class

---
+++ Lec 11.R
@@ -0,0 +1,87 @@
+# 1
+
+emails <- readRDS("email.rds")
+
+print("\"")
+
+cat("\"")
+
+cat("ab\ncd")
+
+cat('a\tb\nc\td')
+
+# 2
+cat(":-\\")
+
+cat("(^_^\")")
+
+cat("@_'-'")
+
+cat("\\m/")
+
+# 3
+library(stringr)
+?str_locate
+?str_sub
+
+# 4
+str_locate(string = "great", pattern = "a")
+
+list = c("great", "fantastic", "super")
+str_locate(string = list, pattern = "a")
+
+occurance = str_locate_all(string = list, pattern = "a")
+
+occurance[1]
+#occurance[1,1]
+occurance[[1]]
+
+### or
+occurance[[1]][1]
+
+#5
+str_sub(string = "testing", start = 1, end = 3)
+## same has before
+str_sub(string = "testing", end = 3)
+
+str_sub("testing", start = 4, end = str_length("testing"))
+
+str_sub("testing", start = 1, end = 4)
+
+#6
+input <- c("abc", "defg")
+str_sub(input, c(2,3))
+
+input <- c("abc", "defg")
+str_sub(input, c(2,3))
+
+#7
+emails <- readRDS("email(1).rds")
+emails
+emails[1]
+cat(emails[1])
+
+str_locate(string = emails[1], pattern = "\n\n")
+
+#8
+metadata1 = str_sub(string = emails[1], start = 1, end = 841)
+metadata1
+
+body1 = str_sub(string = emails[1], start = 844)
+
+cat(metadata1)
+cat(body1)
+
+#9
+str_split(string = metadata1, pattern = "\n")
+
+#10
+breaks = str_locate(string = emails, pattern = "\n\n" )
+breaks
+
+metadata = str_sub(string = emails, start = 1, end = breaks[,1])
+metadata
+
+body = str_sub(string = emails, start = breaks[,2])
+
+
